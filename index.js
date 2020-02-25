a9527cbc8685dffc31a982537462f6cc35e266c4
vlad-yelnikov <vlad.yelnikov@gmail.com>
2020-02-25 02:53:31 +0200

    add callback hell

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const lamp = new Lamp('xiaomi', 'yellow', new PowerRegulator(100));
-
 const microwave = new Microwave('LG', new PowerRegulator(100));
-
 const house = new House('my house');
+
+house.addDevice(microwave);
+house.addDevice(lamp);
+house.delayedOn('LG', 2000, () =>
+    house.delayedOff('LG', 2000, () =>
+        house.delayedOn('LG', 2000, () =>
+            house.delayedOff('LG', 2000, () => console.log(microwave.state))
+        )
+    )
+);
